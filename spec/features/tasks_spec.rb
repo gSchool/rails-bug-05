9c2145188e212c73d0ae9cfc2dc82b315c032894
Ashley Nguyen <a.nguyen020@gmail.com>
2016-03-01 11:09:25 -0800

    add test for task edit

--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -21,16 +21,17 @@ feature 'Tasks' do
   scenario 'User can edit tasks' do
     user = create_user email: "user@example.com"
     task_list = create_task_list(name: "Work List")
-    task = create_task(task_list: task_list, description: "Some task", due_date: 2.days.from_now)
+    task = create_task(task_list: task_list, description: "Some task", due_date: 1.day.from_now)
+    task2 = create_task(task_list: task_list, description: "Another task", due_date: 2.days.from_now)
 
     login(user)
-    click_on "edit"
-    fill_in "Description", with: "Updated task"
+    first('.task').click_link('edit')
+    fill_in "Description", with: "Super awesome task"
     click_on "Update Task"
 
     expect(page).to have_no_content("Some task")
-    expect(page).to have_content("Updated task")
-    expect(task.reload).to eq(task)
+    expect(page).to have_content("Super awesome task")
+    expect(task2.reload).to eq(task2)
   end
 
 end
